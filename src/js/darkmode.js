3b3dcc517aebc8592dda070e71365127d2623cf1
mijin <mmcho122@gmail.com>
2022-06-07 03:15:28 +0900

    🎨 gnb-nav 컴포넌트 cheatsheet 모달 적용

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -1,4 +1,5 @@
 const headerArea = document.querySelector(".gnb-header");
+const cheatsheetModal = document.querySelector(".modal-menu");
 const searchModal = document.querySelector(".modal-history");
 const pageBody = document.querySelector("body");
 const upBtn = document.querySelector(".btn-up");
@@ -10,12 +11,14 @@ const checkDarkMode = () => {
   if (currentMode == "true") {
     isDark = true;
     headerArea.classList.add("on");
+    cheatsheetModal.classList.add("on");
     searchModal.classList.add("on");
     pageBody.classList.add("on");
     upBtn.classList.add("on");
   } else {
     isDark = false;
     headerArea.classList.remove("on");
+    cheatsheetModal.classList.remove("on");
     searchModal.classList.remove("on");
     pageBody.classList.remove("on");
     upBtn.classList.remove("on");
@@ -25,6 +28,7 @@ checkDarkMode();
 
 const handleDarkMode = () => {
   headerArea.classList.toggle("on");
+  cheatsheetModal.classList.toggle("on");
   searchModal.classList.toggle("on");
   pageBody.classList.toggle("on");
   upBtn.classList.toggle("on");
