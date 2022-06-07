e7875089762e02720b988b122e0571681b3548d2
Sooyoung Yoon <ayoooo240@gmail.com>
2022-06-07 21:05:19 +0900

    ✨ 스크롤 위치 인식한 사이드 메뉴 토글 기능 추가

--- src/components/drawer-menu.js
+++ src/components/drawer-menu.js
@@ -2,5 +2,15 @@ const btnDrawer = document.querySelector('.btn-drawer');
 const asideArea = document.querySelector('.aside-area');
 
 btnDrawer.addEventListener('click', () => {
+  btnDrawer.classList.toggle('toggle');
   asideArea.classList.toggle('toggle');
-})
+});
+
+window.addEventListener('scroll', () => {
+  if (body.classList.contains('scroll-down')) {
+    btnDrawer.classList.add('scroll-down');
+  }
+  if (body.classList.contains('scroll-up')) {
+    btnDrawer.classList.remove('scroll-down');
+  }
+});
