dfc1126d7f9802ac15ece6e2ffdfa11333e75d98
whara123 <64121533+whara123@users.noreply.github.com>
2022-06-18 14:25:53 +0900

    🔥 쓰지 않는 코드 삭제

--- src/js/search.js
+++ src/js/search.js
@@ -1,74 +1,74 @@
 // Modal
-const searchData = localStorage.getItem('searchHistoryData');
+const searchData = localStorage.getItem("searchHistoryData");
 let searchHistory = [];
 if (searchData !== null) {
   searchHistory = JSON.parse(searchData);
 }
 
-const searchInputs = document.querySelectorAll('.search-input');
-const searchToggle = document.querySelector('.mobile-search');
-const searchMobileModal = document.querySelector('.search-modal-mobile');
-const searchModalCloseBtn = searchMobileModal.querySelector('.close-btn');
-const searchOverlay = document.querySelector('.search-overlay');
+const searchInputs = document.querySelectorAll(".search-input");
+const searchToggle = document.querySelector(".mobile-search");
+const searchMobileModal = document.querySelector(".search-modal-mobile");
+const searchModalCloseBtn = searchMobileModal.querySelector(".close-btn");
+const searchOverlay = document.querySelector(".search-overlay");
 
 searchInputs.forEach((input) =>
-  input.addEventListener('click', () => {
-    searchModal.classList.add('clicked');
+  input.addEventListener("click", () => {
+    searchModal.classList.add("clicked");
     createHistory(searchHistoryList);
     createHistory(mobileSearchHistoryList);
   })
 );
 
 function openSearchModal() {
-  if (!searchMobileModal.classList.contains('clicked')) {
-    searchMobileModal.classList.add('clicked');
-    searchOverlay.classList.add('clicked');
-    window.addEventListener('click', closeSearchModal);
+  if (!searchMobileModal.classList.contains("clicked")) {
+    searchMobileModal.classList.add("clicked");
+    searchOverlay.classList.add("clicked");
+    window.addEventListener("click", closeSearchModal);
     createHistory(searchHistoryList);
     createHistory(mobileSearchHistoryList);
   }
 }
 
-searchToggle.addEventListener('click', openSearchModal);
+searchToggle.addEventListener("click", openSearchModal);
 
 function closeSearchModalBtn() {
-  searchMobileModal.classList.remove('clicked');
-  searchOverlay.classList.remove('clicked');
+  searchMobileModal.classList.remove("clicked");
+  searchOverlay.classList.remove("clicked");
   createHistory(searchHistoryList);
   createHistory(mobileSearchHistoryList);
 }
 
-searchModalCloseBtn.addEventListener('click', closeSearchModalBtn);
+searchModalCloseBtn.addEventListener("click", closeSearchModalBtn);
 
 function closeSearchModal(e) {
-  if (e.target.className === 'overlay search-overlay clicked') {
-    searchMobileModal.classList.remove('clicked');
-    searchOverlay.classList.remove('clicked');
-    window.removeEventListener('click', closeSearchModal);
+  if (e.target.className === "overlay search-overlay clicked") {
+    searchMobileModal.classList.remove("clicked");
+    searchOverlay.classList.remove("clicked");
+    window.removeEventListener("click", closeSearchModal);
   }
 }
 
-document.addEventListener('click', (e) => {
+document.addEventListener("click", (e) => {
   if (
-    e.target.classList.value !== 'search-input' &&
-    e.target.classList.value !== 'btn-del' &&
-    e.target.classList.value !== 'btn-del-all'
+    e.target.classList.value !== "search-input" &&
+    e.target.classList.value !== "btn-del" &&
+    e.target.classList.value !== "btn-del-all"
   ) {
-    searchModal.classList.remove('clicked');
+    searchModal.classList.remove("clicked");
   }
 
-  if (e.target.classList.value == 'btn-del') {
+  if (e.target.classList.value == "btn-del") {
     e.preventDefault();
     const inText = e.target.previousSibling.innerText;
     searchHistory = searchHistory.filter((text) => {
       return text !== inText;
     });
-    localStorage.setItem('searchHistoryData', JSON.stringify(searchHistory));
+    localStorage.setItem("searchHistoryData", JSON.stringify(searchHistory));
     createHistory(searchHistoryList);
     createHistory(mobileSearchHistoryList);
   }
 
-  if (e.target.classList.value == 'list-item') {
+  if (e.target.classList.value == "list-item") {
     touchList(e.target.firstChild.innerText);
   }
 });
@@ -79,15 +79,18 @@ const touchList = (touchListText) => {
       return text !== touchListText;
     });
     searchHistory.unshift(touchListText);
-    localStorage.setItem('searchHistoryData', JSON.stringify(searchHistory));
+    localStorage.setItem("searchHistoryData", JSON.stringify(searchHistory));
   }
 };
 
-const searchBtns = document.querySelectorAll('.search-btn');
+const searchBtns = document.querySelectorAll(".search-btn");
 for (let i = 0; i < searchBtns.length; i++) {
-  searchBtns[i].addEventListener('click', (e) => {
+  searchBtns[i].addEventListener("click", (e) => {
     e.preventDefault();
-    if (!searchHistory.includes(searchInputs[i].value) && searchInputs[i].value) {
+    if (
+      !searchHistory.includes(searchInputs[i].value) &&
+      searchInputs[i].value
+    ) {
       if (searchHistory.length > 4) {
         searchHistory.pop();
         searchHistory.unshift(searchInputs[i].value);
@@ -100,24 +103,23 @@ for (let i = 0; i < searchBtns.length; i++) {
       });
       searchHistory.unshift(searchInputs[i].value);
     }
-    localStorage.setItem('search', searchInputs[i].value);
-    localStorage.setItem('searchHistoryData', JSON.stringify(searchHistory));
+    localStorage.setItem("searchHistoryData", JSON.stringify(searchHistory));
     location.href = `http://${location.host}/search/?q=${searchInputs[i].value}`;
   });
 }
 
-const searchDataDeleteBtn = document.querySelectorAll('.btn-del-all');
+const searchDataDeleteBtn = document.querySelectorAll(".btn-del-all");
 searchDataDeleteBtn.forEach((deleteBtn) => {
-  deleteBtn.addEventListener('click', () => {
+  deleteBtn.addEventListener("click", () => {
     searchHistory = [];
-    localStorage.setItem('searchHistoryData', JSON.stringify(searchHistory));
+    localStorage.setItem("searchHistoryData", JSON.stringify(searchHistory));
     createHistory(searchHistoryList);
     createHistory(mobileSearchHistoryList);
   });
-})
+});
 
-const searchHistoryList = document.querySelector('.list-history');
-const mobileSearchHistoryList = document.querySelector('.list-history-m');
+const searchHistoryList = document.querySelector(".list-history");
+const mobileSearchHistoryList = document.querySelector(".list-history-m");
 const removeChildAll = (ele) => {
   while (ele.hasChildNodes()) {
     ele.removeChild(ele.firstChild);
@@ -127,22 +129,22 @@ const createHistory = (list) => {
   removeChildAll(list);
   if (searchHistory.length > 0) {
     searchHistory.forEach((v) => {
-      const searchList = document.createElement('li');
-      const listItem = document.createElement('a');
+      const searchList = document.createElement("li");
+      const listItem = document.createElement("a");
 
-      const searchText = document.createElement('strong');
+      const searchText = document.createElement("strong");
       searchText.appendChild(document.createTextNode(v));
 
-      const deleteBtn = document.createElement('button');
-      const deleteImage = document.createElement('img');
+      const deleteBtn = document.createElement("button");
+      const deleteImage = document.createElement("img");
 
-      listItem.setAttribute('class', 'list-item');
-      listItem.setAttribute('href', `/search/?q=${v}`);
-      searchText.setAttribute('class', 'txt-item');
-      deleteBtn.setAttribute('class', 'btn-del');
-      deleteImage.setAttribute('src', '/src/assets/images/icon-close.svg');
-      deleteImage.setAttribute('alt', '삭제버튼');
-      deleteImage.setAttribute('class', 'close-img');
+      listItem.setAttribute("class", "list-item");
+      listItem.setAttribute("href", `/search/?q=${v}`);
+      searchText.setAttribute("class", "txt-item");
+      deleteBtn.setAttribute("class", "btn-del");
+      deleteImage.setAttribute("src", "/src/assets/images/icon-close.svg");
+      deleteImage.setAttribute("alt", "삭제버튼");
+      deleteImage.setAttribute("class", "close-img");
 
       deleteBtn.appendChild(deleteImage);
 
